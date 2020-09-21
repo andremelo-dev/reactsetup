9c560b4b55840000edada7699a5af94a7c995665
André Melo <andremelo.web@gmail.com>
2020-09-21 12:15:04 -0300

    start

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,12 @@
+/* eslint @typescript-eslint/explicit-module-boundary-types: "off" */
+import useSWR from 'swr'
+import api from '../services/api'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useFetch<Data = any, Error = any>(url: string) {
+  const { data, error } = useSWR<Data, Error>(url, async url => {
+    const response = await api.get(url)
+    return response.data
+  })
+  return { data, error }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+
+import ReactLogo from '../assets/react.svg'
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+      <ReactLogo />
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure made by André Melo</p>
+    </Container>
+  )
+}
+
+export default Home
